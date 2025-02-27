a7bf23cc3f512d6f7a05183512d48452b36815e4
Aaron <aaronkoira@gmail.com>
2025-02-28 00:15:46 +0200

    map editor stuff

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -59,9 +59,6 @@ public class TileManager {
             System.out.println("Exception!");
         }
         loadMap("/res/maps/sample.txt", 0);
-
-/*         loadMap("/res/maps/worldmap02.txt", 0);
-        loadMap("/res/maps/testmap01.txt", 1); */
     }
 
     public void getTileImage() {
@@ -80,88 +77,6 @@ public class TileManager {
 
             setup(i, fileName, collision);
         }
-
-        // PLACEHOLDER
-
-/*         setup(0, "grass1", false);
-        setup(1, "grass1", false);
-        setup(2, "placeholder", false);
-        setup(3, "placeholder", false);
-        setup(4, "placeholder", false);
-        setup(5, "placeholder", false);
-        setup(6, "placeholder", false);
-        setup(7, "placeholder", false);
-        setup(8, "placeholder", false);
-        setup(9, "placeholder", false);
-
-        // GRASS
-
-        setup(10, "grass1", false);
-        setup(11, "grass2", false);
-        setup(12, "grass3", false);
-        setup(22, "grass4", false);
-
-        setup(14, "gwbottom", true);
-        setup(15, "gwleft", true);
-        setup(16, "gwright", true);
-        setup(17, "gwtop", true);
-        setup(18, "gwtopleft", true);
-        setup(19, "gwtopright", true);
-        setup(20, "gwbottomleft", true);
-        setup(21, "gwbottomright", true);
-
-        // WATER
-
-        setup(13, "water2", true);
-        setup(58, "water1", true);
-
-        // ROCK
-
-        setup(23, "rock1", true);
-        setup(24, "rock2", true);
-
-        setup(25, "bigrocktl", true);
-        setup(26, "bigrocktr", true);
-        setup(27, "bigrockbl", true);
-        setup(28, "bigrockbr", true);
-
-        // TREE
-
-        setup(29, "tree1tl", true);
-        setup(30, "tree1tr", true);
-        setup(31, "tree1bl", true);
-        setup(32, "tree1br", true);
-
-        setup(33, "tree2tl", true);
-        setup(34, "tree2tr", true);
-        setup(35, "tree2bl", true);
-        setup(36, "tree2br", true);
-
-        setup(37, "tree3tl", true);
-        setup(38, "tree3tr", true);
-        setup(39, "tree3ml", true);
-        setup(40, "tree3mr", true);
-        setup(41, "tree3bl", true);
-        setup(42, "tree3br", true);
-
-        setup(43, "tree4tl", true);
-        setup(44, "tree4tm", true);
-        setup(45, "tree4tr", true);
-        setup(46, "tree4ml", true);
-        setup(47, "tree4mm", true);
-        setup(48, "tree4mr", true);
-        setup(49, "tree4bl", true);
-        setup(50, "tree4bm", true);
-        setup(51, "tree4br", true);
-
-        // GODDESS STATUE
-
-        setup(52, "goddess1_tl", true);
-        setup(53, "goddess1_tr", true);
-        setup(54, "goddess1_ml", true);
-        setup(55, "goddess1_mr", true);
-        setup(56, "goddess1_bl", true);
-        setup(57, "goddess1_br", true); */
     }
 
 public void setup(int index, String imageName, boolean collision){
